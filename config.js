531bd0cf59716f4c3f52f1db1ed9253c6cb64224
Fedor Paretsky <me@f3d0r.com>
2018-09-19 17:26:42 -0700

    Update config and package, remove old .js scripts

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 module.exports = {
     FILES: {
         LOCAL_PROXY_FILE_NAME: "exported-proxies.txt",
-        LOCAL_URL_LIST: "parkopedia_urls.txt",
+        LOCAL_URL_LIST: "parkopedia-urls.txt",
     },
-    ACCESS_TOKENS: {
-        MAPBOX: '***REMOVED***'
-    }
 }
