84ebe90be4ec6b51aa347b031374cd378a9de478
Fedor Paretsky <me@f3d0r.com>
2018-09-19 23:50:31 -0700

    Add database packages, sql insertion formatting, responses parsing/combination

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ process.setMaxListeners(0);
 
 const puppeteer = require('puppeteer');
 var fs = require('fs');
-const config = require('./config');
+const config = require('./config/');
 const localURLListName = config.FILES.LOCAL_URL_LIST;
+var flatten = require('flat')
 
 startScript()
 
@@ -39,7 +40,7 @@ function startScript() {
             throw err;
         } else {
             parkopediaURLs = data.split("\n");
-            getParkpodiaData(function () {
+            getParkopediaData(function () {
                 console.log("DONE WRITING FILES - MOVING TO PARSE/COMBINE FILES");
                 combineJSON();
             }, function (error) {
@@ -49,7 +50,7 @@ function startScript() {
     });
 }
 
-function getParkpodiaData(successCB, failCB) {
+function getParkopediaData(successCB, failCB) {
     var reqs = [];
     parkopediaURLs.forEach(function (currentURL) {
         reqs.push(scrape(currentURL)
@@ -96,10 +97,78 @@ function combineJSON() {
         });
         Promise.all(fileReqs)
             .then(function (fileContents) {
-                // console.log(fileContents)
+                finalContents = [];
+                fileContents.forEach(function (currentContent) {
+                    finalContents.push(selectRelevantContent(currentContent, currentContent.data.refData.features, currentContent.data.refData.ctype, currentContent.data.refData.grestr));
+                });
+                writeFileContents = "";
+                finalContents.forEach(function (current1) {
+                    current1.forEach(function (current2) {
+                        writeFileContents += current2 + "\n";
+                    })
+                });
             })
             .catch(function (error) {
                 console.log(error)
             });
     });
+}
+
+function selectRelevantContent(content, facilityKeys, paymentTypeKeys, restrictionKeys) {
+    var finalContent = [];
+    content.locations.all.forEach(function (currentSpot) {
+        var id, lng, lat, pretty_name, payment_process, payment_types, restrictions, surface_type, address, city, country, capacity, facilities, phone_number, url = "";
+        var pricing_json, paybyphone = {};
+        currentSpot.features.forEach(function (currentFeature) {
+            if (typeof currentFeature.properties != "undefined" && typeof currentFeature.properties.feature_type != "undefined" && currentFeature.properties.feature_type == "position") {
+                if (lat != "" && lng != "") {
+                    lng = currentFeature.geometry.coordinates[0];
+                    lat = currentFeature.geometry.coordinates[1];
+                }
+            }
+            if (typeof currentFeature.properties != "undefined" && typeof currentFeature.properties.name != "undefined" && currentFeature.properties.name != "") {
+                id = currentFeature.properties.id;
+                pretty_name = currentFeature.properties.name;
+
+                rawPayments = currentFeature.properties.payment_types;
+                payment_types = [];
+                try {
+                    for (var index = 0; index < rawPayments.length; index++) {
+                        payment_types.push(paymentTypeKeys[rawPayments[index]]);
+                    }
+                } catch (e) {}
+
+                rawRestrictions = currentFeature.properties.restrictions;
+                restrictions = [];
+                for (var index = 0; index < rawRestrictions.length; index++) {
+                    restrictions.push(restrictionKeys[rawRestrictions[index]]);
+                }
+
+                surface_type = currentFeature.properties.surface_type;
+                address = "";
+                currentFeature.properties.address.forEach(function (current) {
+                    address += (current + " ");
+                })
+                address = address.trim();
+
+                city = currentFeature.properties.city;
+                country = currentFeature.properties.country;
+                capacity = currentFeature.properties.capacity;
+                payment_process = currentFeature.properties.payment_process;
+
+                rawFacilities = currentFeature.properties.facilities;
+                facilities = [];
+                for (var index = 0; index < rawFacilities.length; index++) {
+                    facilities.push(facilityKeys[rawFacilities[index]]);
+                }
+
+                phone_number = currentFeature.properties.phone;
+                url = currentFeature.properties.url;
+                pricing_json = currentFeature.properties.prices;
+                paybyphone = currentFeature.properties.paybyphone;
+            }
+        });
+        finalContent.push([id, lng, lat, pretty_name, pricing_json, payment_process, payment_types, restrictions, surface_type, address, city, country, paybyphone, capacity, facilities, phone_number, url]);
+    });
+    return finalContent;
 }
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    FILES: {
-        LOCAL_PROXY_FILE_NAME: "exported-proxies.txt",
-        LOCAL_URL_LIST: "parkopedia-urls.txt",
-    },
-}
